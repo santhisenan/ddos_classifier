2b2ae0f889edea5dc99da697150d95a2fd9a9fc8
santhisenan <santhisenan@outlook.com>
2019-02-04 10:44:24 +0530

    Started expoerimenting with different models

--- preprocess_dataset.py
+++ preprocess_dataset.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[24]:
-
-
 import numpy as np
 import pandas as pd
 
@@ -16,46 +13,10 @@ from keras.utils import np_utils
 import matplotlib.pyplot as plt
 get_ipython().run_line_magic('matplotlib', 'inline')
 
-
-# In[13]:
-
-
 data = pd.read_csv('balanced_dataset.csv')
-
-
-# In[14]:
-
-
-print(data.head())
-
-
-# In[15]:
-
-
-print(data.columns)
-
-
-# In[16]:
-
 
 data.drop("Unnamed: 0", axis=1, inplace=True)
 data.drop("Unnamed: 0.1", axis=1, inplace=True)
-
-
-# In[17]:
-
-
-data.columns
-
-
-# In[18]:
-
-
-data[data.dtypes[(data.dtypes=="float64")|(data.dtypes=="int64")]
-                        .index.values].hist(figsize=[11,11])
-
-
-# In[19]:
 
 
 dummy_pkt_type = pd.get_dummies(data['PKT_TYPE'])
@@ -67,16 +28,6 @@ data = pd.concat([data, dummy_flags], axis = 1)
 data.drop("NODE_NAME_FROM",axis=1,inplace=True)
 data.drop("NODE_NAME_TO",axis=1,inplace=True)
 
-
-# In[20]:
-
-
-data.columns
-
-
-# In[21]:
-
-
 features = ['SRC_ADD', 'DES_ADD', 'PKT_ID', 'FROM_NODE', 'TO_NODE', 'PKT_SIZE',
        'FID', 'SEQ_NUMBER', 'NUMBER_OF_PKT', 'NUMBER_OF_BYTE', 'PKT_IN',
        'PKT_OUT', 'PKT_R', 'PKT_DELAY_NODE', 'PKT_RATE', 'BYTE_RATE',
@@ -86,35 +37,15 @@ features = ['SRC_ADD', 'DES_ADD', 'PKT_ID', 'FROM_NODE', 'TO_NODE', 'PKT_SIZE',
 X = data[features].values
 Y = data['PKT_CLASS']
 
-print(X.shape)
-print(Y.shape)
-
-
-# In[22]:
-
-
 encoder = LabelEncoder()
 encoder.fit(Y)
 encoded_Y = encoder.transform(Y)
-
-
-# In[25]:
-
-
 dummy_Y = np_utils.to_categorical(encoded_Y)
-
-
-# In[28]:
-
 
 lsvc = LinearSVC(C=0.01, penalty="l1", dual=False).fit(X, encoded_Y)
 model = SelectFromModel(lsvc, prefit=True)
 X_new = model.transform(X)
 X_new.shape
-
-
-# In[29]:
-
 
 scalar = StandardScaler(copy=True, with_mean=True, with_std=True)
 scalar.fit(X_new)
